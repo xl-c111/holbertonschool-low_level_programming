21f97f99354c439b90ffe830050201cbd7b7a406
xl-c111 <10430@holbertonstudents.com>
2025-02-16 14:55:55 +0000

    prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints using printf.
+ *
+ * Description: This program prints exactly "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line using the printf function.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
