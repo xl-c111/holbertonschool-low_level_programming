cf1fa02b819616dbfe43ec5730793c20eaf7371f
xl-c111 <10430@holbertonstudents.com>
2025-02-16 14:49:15 +0000

    fix typo

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/*
  * main - Prints a message.
  *
  * Description: Prints "\"Programming is like building a multilingual puzzle"
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-p:uts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
