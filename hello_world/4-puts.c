ddb0b734084620effc96c20b8cfd731169654acd
xl-c111 <10430@holbertonstudents.com>
2025-02-16 14:43:54 +0000

    add documentation for the function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+* main prints "Programming is like building a multilingual puzzle
+* followed by a new line.
+*
+* Return: 0.
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
