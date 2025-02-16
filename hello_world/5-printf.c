2a76ce84e48de8ef3add4c325da5edec91b41025
xl-c111 <10430@holbertonstudents.com>
2025-02-16 15:00:52 +0000

    fix character length

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,13 +3,16 @@
 /**
  * main - Prints using printf.
  *
- * Description: This program prints exactly "with proper grammar, but the outcome is a piece of art,"
- * followed by a new line using the printf function.
- *
+ * Description: This program prints exactly 
+ * "with proper grammar, but the outcome is 
+ * a piece of art,"followed by a new 
+ * line using the printf function.
+ *i
  * Return: 0.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a " 
+"piece of art,\n");
 return (0);
 }
