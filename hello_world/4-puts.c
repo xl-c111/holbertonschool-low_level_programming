2b97b68772091b37cadbd173959a4b9d6025c88f
xl-c111 <10430@holbertonstudents.com>
2025-02-16 14:47:03 +0000

    add description

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
-* main prints "Programming is like building a multilingual puzzle
-* followed by a new line.
-*
-* Return: 0.
-*/
+ * main - Prints a message.
+ *
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * followed by a new line.
+ *
+ * Return: 0.
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+p:uts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
