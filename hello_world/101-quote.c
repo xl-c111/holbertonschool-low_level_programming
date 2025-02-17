dfdbac24bd53f8260be3eecc4df33615882095dc
xl-c111 <10430@holbertonstudents.com>
2025-02-17 14:10:48 +0000

    fix betty style

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -10,6 +10,6 @@
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        write(2, msg, sizeof(msg) - 1); 
-        return (1);
+	write(2, msg, sizeof(msg) - 1);
+	return (1);
 }
