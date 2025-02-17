2180885a331214c6afbf418af7a903e2d8950dd1
xl-c111 <10430@holbertonstudents.com>
2025-02-17 10:59:14 +0000

    update task 4-Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,13 +2,15 @@
 /**
  * main - Prints a message.
  *
- * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Description: print "Programming is like building a multilingual puzzle"
  * followed by a new line.
  *
- * Return: 0.
+ * Return: always 0 (Success)
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
