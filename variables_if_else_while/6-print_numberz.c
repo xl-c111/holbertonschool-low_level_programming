c6fd0d729a4d3dd72f05e49eb701069a5a660f7b
xl-c111 <10430@holbertonstudents.com>
2025-02-18 15:07:34 +0000

    loop over int

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(num);
+		putchar(num + '0');
 	}
 	putchar('\n');
 	return (0);
---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Description: use for loop to print 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	for 
