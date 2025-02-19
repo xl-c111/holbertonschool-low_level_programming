56666ef0aad6d52ef28d4f57bb818b175c5ba7f1
xl-c111 <10430@holbertonstudents.com>
2025-02-19 15:44:47 +0000

    add line breaker

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	_putchar('_');	
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -18,6 +18,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
