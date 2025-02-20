867a03250e179a341f4a5428527a06ce66436443
xl-c111 <10430@holbertonstudents.com>
2025-02-20 09:37:26 +0000

    remove whitespace

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,20 +2,19 @@
 
 /**
  * _islower - check for lowercase character
- *
  * @c: the character to check
- *
  * Return: 1 if c is lowercase, 0 otherwise
  */
+
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{	
-		return 1;
+	{
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
