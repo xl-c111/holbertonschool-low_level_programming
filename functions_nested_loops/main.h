e6ab2bd0bdc1de7776e06df1a73631320ec82183
xl-c111 <10430@holbertonstudents.com>
2025-02-20 11:41:48 +0000

    prints the sign of a number.

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,16 +10,17 @@ int add(int a, int b);
 /* Function prototype for print_alphabet */
 void print_alphabet(void);
 
-/* Function prototype for print_alphabet 10 times*/
+/* Function prototype for print_alphabet 10 times */
 void print_alphabet_x10(void);
 
-/* Function prototype for checking for lowercase character*/
+/* Function prototype for checking for lowercase character */
 int _islower(int c);
 
-/* Function prototype for checking for  alphabetic character*/
+/* Function prototype for checking for alphabetic character */
 int _isalpha(int c);
 
-
+/* Function prototype for printing a sign of number */
+int print_sign(int n);
 
 #endif /* MAIN_H */
 
