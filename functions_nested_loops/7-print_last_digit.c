1a42fa15ff049def2fddab95fb82b78a0555fa54
xl-c111 <10430@holbertonstudents.com>
2025-02-21 03:51:06 +0000

    fix print issue - add _putchar

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,19 +3,26 @@
 /**
  * print_last_digit - print the last digit of number
  * @n: the number to be printed
- * @last_digit: the last digit of n
- * Return: last_digit
+ *
+ * Return: last_digit of n
  */
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
 
-	if (last_digit < 0)
+	if (n >= 0)
 	{
-		return (-last_digit);
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+		return (last_digit);
 	}
 
-	return (last_digit);
+	else
+	{
+		last_digit = -(n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 
 }
