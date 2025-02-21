4ebdcb9b38f1eb8f883a9163633c6916bb41bc9f
xl-c111 <10430@holbertonstudents.com>
2025-02-21 14:47:03 +0000

    fix space

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -25,18 +25,15 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
 					_putchar(result + '0');
 				}
 				else if (result < 99)
 				{
 					_putchar(' ');
-					_putchar(' ');
 					_putchar((result / 10) + '0');
 					_putchar((result % 10) + '0');
 				} else
 				{
-					_putchar(' ');
 					_putchar((result / 100) + '0');
 					_putchar((result / 10 % 10) + '0');
 					_putchar((result % 10) + '0');
