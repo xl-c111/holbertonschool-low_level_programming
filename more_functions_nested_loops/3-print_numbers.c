69e847712c6d66768ca080df59c6497885144cca
xl-c111 <10430@holbertonstudents.com>
2025-02-24 01:19:27 +0000

    print numbers from 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0-9
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototype for _putchar */
+int _putchar(char c);
+
 /* Function for checking for uppercase character */
 int _isupper(int c);
 
@@ -10,7 +13,8 @@ int _isdigit(int c);
 /* Function for multiplying two integers */
 int mul(int a, int b);
 
-
+/* Function for printing the numbers */
+void print_numbers(void);
 
 
 
