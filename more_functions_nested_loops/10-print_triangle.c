c04918d3b4178e22488986ba2e90232489e5b79a
xl-c111 <10430@holbertonstudents.com>
2025-02-24 05:04:51 +0000

    fix the skip for j = size - i - 1

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -14,13 +14,13 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = 0; j < size; j++)
 			{
 				if (j < size - i - 1)
 				{
 					_putchar(' ');
 				}
-				if (j > size - i - 1 && j <= size)
+				if (j >= size - i - 1 && j < size)
 				{
 					_putchar('#');
 				}
