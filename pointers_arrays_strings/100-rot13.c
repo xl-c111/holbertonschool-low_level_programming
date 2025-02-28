6480badad2f1837d5b559b0083d432bda5478d57
xl-c111 <10430@holbertonstudents.com>
2025-02-28 16:49:09 +0000

    encodes a string using rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the input string
+ * Return: pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		if ((*ptr >= 'A' && *ptr <= 'Z') || (*ptr >= 'a' && *ptr <= 'z'))
+		{
+			*ptr = ((*ptr - 'A' + 13) % 26) + 'A' + ((*ptr & 32));
+		}
+		ptr++;
+	}
+	return (str);
+}
