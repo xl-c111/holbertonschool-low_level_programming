98664d6dc7586498bafeda8fa735c11b7e940069
XIA0LING CUI <10430@holbertonstudents.com>
2025-03-04 18:00:40 +1100

    Update 3-strcmp.c
    put if...else... inside while loop

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,22 +2,25 @@
 
 /**
  * _strcmp - compares two strings
- * @s1: the fisrt string
+ * @s1: the first string
  * @s2: the second string
  *
- * Return: an interger representing the difference between the first
+ * Return: an integer representing the difference between the first
  *         different characters in ASCII value, or 0 if strings are equal
  */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (*s1 != *s2)
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else 
 		{
 			return (*s1 - *s2);
 		}
-		s1++;
-		s2++;
 	}
 	return (*s1 - *s2);
 }
