40564e88ced01e07bd7341c3ca6ef2bbd8e4238f
xl-c111 <10430@holbertonstudents.com>
2025-03-04 16:42:19 +0000

    fix - do not allow loop

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,25 +1,36 @@
 #include "main.h"
 /**
- * is_prime_number - to check if an integer is a prime number
+ * is_prime_recursive - to check if an integer is a prime number
  * @n: the input integer
+ * @i: the divisor used in the recursive check
  * Return: if n is a prime number, return 1
  *         otherwise return 0
  *
  */
+int is_prime_recursive(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_recursive(n, i + 1));
+}
+
+/**
+ * is_prime_number - to check if an integer is a prime number
+ * @n: the input integer
+ * Return: if n is a prime number, return 1
+ *         otherwise return 0
+ */
 int is_prime_number(int n)
 {
-	int i = 2;
-
 	if (n < 2)
 	{
 		return (0);
 	}
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_prime_recursive(n, 2));
 }
