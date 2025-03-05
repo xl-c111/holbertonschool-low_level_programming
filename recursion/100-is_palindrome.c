073266ad439d3ad29b5aa53185af6206aa88a2ec
xl-c111 <10430@holbertonstudents.com>
2025-03-05 08:57:55 +0000

    to check if a string is a palindrome

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_palindrome_recursive - recursively check if a string is a palindrome
+ * @s: the input string
+ * @left: the index starting at the beginning (left side)
+ * @right: the index starting at the end (right side)
+ * Return: 1 if the string is a palindrome
+ *         0 if not, otherwise recursively call is_palindrome_recursive
+ *         to check next character
+ */
+int is_palindrome_recursive(char *s, int left, int right)
+{
+	if (left >= right)
+	{
+		return (1);
+	}
+	if (s[left] != s[right])
+	{
+		return (0);
+	}
+	return (is_palindrome_recursive(s, left + 1, right - 1));
+}
+/**
+ * is_palindrome - calculate the length of string and call the helper function
+ *                 to check whether a string is a palindrome
+ * @s: the string to bec checked
+ * Return: 1 if the string is a palindrome, 0 if not
+ *
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+
+	return (is_palindrome_recursive(s, 0, len - 1));
+}
+
