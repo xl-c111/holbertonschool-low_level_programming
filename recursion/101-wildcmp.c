978422137908befc01519b7cfc28645d6a4b07b1
XIA0LING CUI <10430@holbertonstudents.com>
2025-03-08 22:21:23 +1100

    add comments about comparison conditions 101-wildcmp.c
    If s2 has a '*', we try to match with s1 in two ways
    1. Skip '*' and match remaining characters of s2 with s1.
    2. Include the current character of s1 and check again.

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -10,17 +10,22 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 	{
-		return (1);
+		return (1);   //if both strings are empty, identical
 	}
-	if (*s2 == '*')
+	
+	// If s2 has a '*', we try to match with s1 in two ways:
+        // 1. Skip '*' and match remaining characters of s2 with s1.
+        // 2. Include the current character of s1 and check again.
+	
+	if (*s2 == '*')    
 	{
-		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))    // Skip the '*' and move to the next character in s2
 		{
-			return (1);
-		}
-		return (wildcmp(s1, s2 + 1));
+			return (1);                        // Match found by skipping one character in s1
+ 		}
+		return (wildcmp(s1, s2 + 1));              // Or skip the '*' and move to the next character in s2
 	}
-	if (*s1 == *s2)
+	if (*s1 == *s2)        // If characters match or s2 has no wildcard, move to the next character
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
