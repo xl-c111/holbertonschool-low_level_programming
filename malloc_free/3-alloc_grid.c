b1e5c300916d0747bf47a25a30abb5a5b913d6d4
xl-c111 <10430@holbertonstudents.com>
2025-03-11 15:18:38 +0000

    fix memory leak

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * alloc_grid - creates a pointer to a 2D array
+ *              with every element initialized to 0
  * @width: width of the grid
  * @height: height of the grid
- * Return: returns a pointer to a 2 dimensional array of integers
+ * Return: returns a pointer to the allocated 2 dimensional array of integers
  *         NULL if on failure
  *         NULL if width or height is 0 or negative
  */
@@ -13,6 +14,7 @@ int **alloc_grid(int width, int height)
 	int h;
 	int w;
 	int **grid;
+	int i;
 
 	if (width <= 0 || height <= 0)
 	{
@@ -31,9 +33,13 @@ int **alloc_grid(int width, int height)
 
 		if (grid[h] == NULL)
 		{
+			for (i = 0; i < h; i++)
+			{
+				free(grid[i]);
+			}
+			free(grid);
 			return (NULL);
 		}
-
 		for (w = 0; w < width; w++)
 		{
 			grid[h][w] = 0;
