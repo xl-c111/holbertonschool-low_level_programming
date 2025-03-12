78261d1e66477b2032194f93955655aeba2e5360
xl-c111 <10430@holbertonstudents.com>
2025-03-12 05:15:21 +0000

    fix function error

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,23 +3,31 @@
 /**
  * _calloc - allocates memory for an array of nmemb elements of size bytes each
  * @nmemb: number of elements to be allocated
- * @size: size of each element
- * Return: a pointer to the allocated memory
+ * @size: size in bytes of each element
+ * Return: a pointer to the allocated memory, which is set to 0;
  *         NULL if fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *p;
+	char *p;
+	unsigned int i, total_bytes;
+
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	p = (unsigned int *)calloc(nmemb, size);
+
+	total_bytes = nmemb * size;
+	p = malloc(total_bytes);
 
 	if (p == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < total_bytes; i++)
+	{
+		p[i] = 0;
 	}
 	return (p);
 }
