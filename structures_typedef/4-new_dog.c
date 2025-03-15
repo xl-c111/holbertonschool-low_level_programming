18be6132751040e280dc6e4f31eff4faccc89cd6
XIA0LING CUI <10430@holbertonstudents.com>
2025-03-15 21:23:49 +1100

    Update 4-new_dog.c
    add comments

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -42,28 +42,31 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * *new_dog - create a new dog
+ * new_dog - create a new dog
  * @name: a pointer to a character array that stores new dog's name
  * @age: a float represents new dog's age
  * @owner: a pointer to a character array that stores new dog's owner's name
- * Return: a pointer to the newly allacoted dog structure
+ * Return: a pointer to the newly allocated dog_t structure
  *         NULL if fails
  */
 
-dog_t *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)   /* dog_t: a custom data */
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
-
-	if (new_dog == NULL)
+	/* new_dog is declared as a pointer to store the address of the memory block allocated by malloc */
+	dog_t *new_dog = malloc(sizeof(dog_t));  
+	{
+		if (new_dog == NULL)
 		return (NULL);
-
+	}
+	/* Allocated memory for the name field of dog_t structure */
 	new_dog->name = malloc(_strlen(name) + 1);
+	
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
+        /* Allocated memory for the owner field of dog_t structure */
 	new_dog->owner = malloc(_strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
@@ -72,9 +75,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	_strcpy(new_dog->name, name);
-	_strcpy(new_dog->owner, owner);
-	new_dog->age = age;
+	_strcpy(new_dog->name, name);    /* Copy the input name string into the allocated memory pointed to by new_dog->name */
+	_strcpy(new_dog->owner, owner);  /* Copy the input owner string into the allocated memory pointed to by new_dog->owner */
+	new_dog->age = age;              /* Assign the input age value to the age field of the new dog_t structure */
 
 	return (new_dog);
 }
