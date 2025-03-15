dc2f6fff8200972e336a7cd857ea254742fcbac1
xl-c111 <10430@holbertonstudents.com>
2025-03-15 16:15:49 +0000

    initialize k to 0

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -10,7 +10,7 @@
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int i, j, k, total_length = 0;
+	int i, j, k = 0, total_length = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
@@ -27,6 +27,7 @@ char *argstostr(int ac, char **av)
 
 	if (str == NULL)
 		return (NULL);
+	
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
