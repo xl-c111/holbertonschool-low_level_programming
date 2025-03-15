1537a3551691cc47be36fd00dd6dc890ca0a40dc
XIA0LING CUI <10430@holbertonstudents.com>
2025-03-16 02:11:30 +1100

    Update 100-realloc.c
    /* condition ? expression_if_true : expression_if_false */
    copy_size = old_size < new_size ? old_size : new_size

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -5,8 +5,8 @@
 /**
  * _realloc - reallocates a memory block
  * @ptr: a pointer to the memory block previously allocated
- * @old_size: the old size of memory block
- * @new_size: the new size of memory block
+ * @old_size: the old size of the memory block
+ * @new_size: the new size of  the memory block
  * Return: a new pointer point to newly allocated memory;
  *         NULL if allocation fails
  */
@@ -32,14 +32,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_ptr == NULL)
 		return (NULL);
-
-	copy_size = old_size < new_size ? old_size : new_size;
-	src =  ptr;
-	dest = new_ptr;
+	
+	copy_size = old_size < new_size ? old_size : new_size;        /* condition ? expression_if_true : expression_if_false */
+	src =  (char *)ptr;                   /* the void pointer ptr is cast to a char pointer */
+	dest = (char *)new_ptr;               /* the void pointer new_ptr is cast to a char pointer */
 
 	for (i = 0; i < copy_size; i++)
 	{
-		dest[i] = src[i];
+		dest[i] = src[i];             /* in each iteration, it copies 1 byte from src to dest */
 	}
 	free(ptr);
 	return (new_ptr);
