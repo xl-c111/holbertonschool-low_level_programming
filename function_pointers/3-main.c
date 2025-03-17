4afa8b48502da35e44d656e76de4265a9b87d6ce
XIA0LING CUI <10430@holbertonstudents.com>
2025-03-18 00:13:05 +1100

    Update 3-main.c
    op_func = get_op_fun(argv[2]) 
    argv[2] is third command-line, --->>> operator 
    argv[2] is a string with one single char, ending with '\0'
    argv[2][0] is the operator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,8 +4,12 @@
 #include "3-calc.h"
 /**
  * main - performs simple operations
- * @argc: argument number
- * @argv: argument vector
+ * @argc: argument number (4 command lines in this program)
+ * @argv: argument vector 
+ *        argv[0]: the name of program - ./calc 
+ *	  argv[1]: the first operand
+ *        argv[2]: the operator
+ *	  argv[3]: the second operand
  * Return: Always 0 (Success)
  *         98 if the number of arguments is wrong
  *         99 if the operator is none of the above
@@ -14,29 +18,36 @@
 int main(int argc, char **argv)
 {
 	int a, b;
-	int (*op_func)(int, int);
+	int (*op_func)(int, int);  /* declare a func ptr named op_func, point to any function that takes two int arguments*/
 
-	if (argc != 4)
+	if (argc != 4)              
 	{
 		printf("Error\n");
 		return (98);
 	}
-	op_func = get_op_func(argv[2]);
-
-	if (!op_func)
+	
+	/* int (*get_op_func(char *s))(int, int) and int (*op_func)(int, int) both return a func ptr type of int (*)(int, int) */
+	/* argv[2] is a string and contains only 1 single operator char */
+	/* get_op_func accepts argv[2] as an argument, to determine which arithmetic operation is used */
+	/* get_op_func returns the ptr to that function, op_func stores the address of that arithmetic function (like op_add) */
+	
+	op_func = get_op_func(argv[2]);   
+	
+	if (!op_func)            /* checks if op_func is NULL(if get_op_func didn't find a matching operation) */
 	{
 		printf("Error\n");
 		return (99);
 	}
 
-	a = atoi(argv[1]);
+	a = atoi(argv[1]);       /* converts first and third command-line arguments from string to int */
 	b = atoi(argv[3]);
-
+        
+	/* argv[2][0] is a string, starting with one single char, ending with '\0', argv[2][0] is the operator */
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 	{
 		printf("Error\n");
 		return (100);
 	}
-	printf("%d\n", op_func(a, b));
+	printf("%d\n", op_func(a, b));       /* calls the function pointed to by op_func with the operands a and b */
 	return (0);
 }
