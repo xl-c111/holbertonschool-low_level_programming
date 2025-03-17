036db70bec411a864e6204e2b313d866bd417e0a
XIA0LING CUI <10430@holbertonstudents.com>
2025-03-17 22:19:52 +1100

    Update 3-calc.h

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -11,7 +11,7 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-} op_t;
+} op_t;                /* op_t is alias name for the structure struct op */
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
