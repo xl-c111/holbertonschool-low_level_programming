40cc81dad129f9acedc88fc458efb5b915b105e2
xl-c111 <10430@holbertonstudents.com>
2025-03-19 13:58:03 +0000

    task 3 To be is a to be the value of a variable

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @
+ *
+ *
+ *
+ *
+ *
+ */
+void print_all(const char * const format, ...)
