50ccc61a51dfdf19d23e591d15fe0d820ad8a98c
xl-c111 <10430@holbertonstudents.com>
2025-03-19 13:14:26 +0000

    remove early return

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,8 +18,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		value = va_arg(args, int);
