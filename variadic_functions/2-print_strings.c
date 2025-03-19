58e24896710adfe2ec03dc65ca332f77cefd46e7
xl-c111 <10430@holbertonstudents.com>
2025-03-19 13:46:46 +0000

    (nil)should be with parentheses

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		if (value == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 
 		if (separator != NULL && i < n - 1)
@@ -35,6 +35,4 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	printf("\n");
 	va_end(args);
-
-
 }
