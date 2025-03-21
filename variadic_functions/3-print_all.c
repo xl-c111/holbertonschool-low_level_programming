4280f3e5923d38ede53ed3979de282b20348fa57
XIA0LING CUI <10430@holbertonstudents.com>
2025-03-22 01:09:10 +1100

    Update 3-print_all.c
    add comments

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,34 +4,39 @@
 #include "variadic_functions.h"
 /**
  * print_all - prints anything
- * @format: a list of types of arguments passed to the function
+ * @format: is a list of types of arguments passed to the function(a string,
+            each char in format str represents a type: c for char, f for float...)
  * Return: nothing
  */
 
 void print_all(const char * const format, ...)
 {
-	print_t ops[] = {
-		{"c", print_char},
+	print_t ops[] = {            /* an array of print_t structures named ops */
+		{"c", print_char},   /* each element of the array consists of two parts: format specifier and corresponding print function */
 		{"f", print_float},
 		{"i", print_int},
 		{"s", print_str}
 	};
 
-	int i = 0;
-	int j;
+	int i = 0;                  /* index to iterate over the format string (cfis) */
+	int j;                      /* index to iterate over the array ops (4 elements) */
 	char *separator = "";
 
-	va_list args;
+	va_list args;               /* declare a variable args of type va_list */
 
-	va_start(args, format);
+	va_start(args, format);     /* initialize the variable args to point to the first argument after format */
 
-	while (format != NULL && format[i] != '\0')
+	while (format != NULL && format[i] != '\0') /* ensure format str is valid and the current char is not null */
 	{
 		j = 0;
 
-		while (j < 4)
+		while (j < 4)        
 		{
-			if (format[i] == ops[j].specifier[0])
+			/* check if the current char in format str matches the first char of specifier in the current ops[j] element*/
+			/* print the current separator string, initially is an empty str */
+			/* call the print function associated with the current specifier */
+			/* set the separator variable to ", " */
+			if (format[i] == ops[j].specifier[0])   
 			{
 				printf("%s", separator);
 				ops[j].print_func(args);
@@ -51,7 +56,8 @@ void print_all(const char * const format, ...)
  */
 void print_char(va_list args)
 {
-	int c = va_arg(args, int);
+	/* arguments passed through an ellipsis (...), types such as char and short are automatically promoted to int */
+	int c = va_arg(args, int);  
 
 	printf("%c", c);
 }
@@ -61,7 +67,7 @@ void print_char(va_list args)
  */
 void print_float(va_list args)
 {
-	double f = va_arg(args, double);
+	double f = va_arg(args, double);    /* float is automatically promoted to double */
 
 	printf("%f", f);
 }
