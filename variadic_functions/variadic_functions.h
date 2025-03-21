7c6ddd133611dbcaf4fd301d52db7ebfbd30513b
XIA0LING CUI <10430@holbertonstudents.com>
2025-03-21 23:55:50 +1100

    Update variadic_functions.h
    add commet
    char *specifier: a str to store format specifier
    void (*print_func)(va_list args): a func ptr points any func that takes a va_list argument and returns nothing
    va_list: a type used for handling variable argument lists
    args: a name given to va_list

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -11,17 +11,18 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 int _putchar(char c);
 
-void print_char(va_list args);
+void print_char(va_list args);     
 void print_float(va_list args);
 void print_int(va_list args);
 void print_str(va_list args);
 
 void print_all(const char * const format, ...);
 
-typedef struct print
+typedef struct print                       /* define a structure called print */ 
 {
-	char *specifier;
-	void (*print_func)(va_list args);
+	char *specifier;                   /* first member: a pointer to a char str, is used to store format specifier*/
+	void (*print_func)(va_list args);  /* second member: a func ptr, points to any func that matches the signature: */
+                                                            /* takes a va_list argument and returns nothing */
 } print_t;
 
 #endif
