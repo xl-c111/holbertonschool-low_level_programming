b0c160ee01151de0014ecc9088a987b2b944b2c9
xl-c111 <10430@holbertonstudents.com>
2025-04-01 13:43:24 +0000

    fix memory leak issue

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -8,13 +8,14 @@
 void free_list(list_t *head)
 {
 	list_t *temp;
-	
+
 	while (head != NULL)
 	{
 		temp = head;
 		head = temp->next;
+
+		if (temp->str)
+			free(temp->str);
+		free(temp);
 	}
-	if (temp->str)
-		free(temp->str);
-	free(temp);
 }
