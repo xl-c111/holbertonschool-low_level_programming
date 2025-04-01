2a8a8ad1c34a11cb86268176a4abebe56fc44cd2
xl-c111 <10430@holbertonstudents.com>
2025-04-01 01:44:53 +0000

    prints all the elements of a list_t list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,19 +1,22 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
+/**
+ * struct Node - define a new structure struct Node 
+ * @str: a string
+ * @len: length of the string
+ * @next: a poniter to the next node
+ * @list_t: alias of the structure
+ */
+
+typedef struct Node
+{
+        char *str;
+        unsigned int len;
+        struct Node *next;
+}list_t;
+
 size_t print_list(const list_t *h);
-
-size_t list_len(const list_t *h);
-
-list_t *add_node(list_t **head, const char *str);
-
-list_t *add_node_end(list_t **head, const char *str);
-
-void free_list(list_t *head);
-
-int _putchar(char c);
-
-
-
 
 #endif
