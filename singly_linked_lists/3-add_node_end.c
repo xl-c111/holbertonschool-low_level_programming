92079ac36a34eb7a904a67be95a37ee774c7f2a4
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-02 19:45:44 +1100

    Update 3-add_node_end.c
    while (temp-> next != NULL) temp = temp->next; 
    loop continues until temp reaches the last node of the list
    temp->next = newNode, sets next ptr of temp(the current last node) to the new Node. 
    newNode = temp->next,  it would reassign newNode to the value of the last node's next pointer (usually NULL)

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -25,38 +25,39 @@ int _strlen(const char *str)
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newNode;
-	list_t *temp;
+	list_t *newNode;                            /* used to store the new node*/
+	list_t *temp;                               /* used to traverse the linked list */
 
 	if (head == NULL || str == NULL)
 		return (NULL);
 
-	newNode = malloc(sizeof(list_t));
+	newNode = malloc(sizeof(list_t));           /* allocates memory for the new node with the size of list_t structure */ 
 	if (newNode == NULL)
 		return (NULL);
-	newNode->str = strdup(str);
+	newNode->str = strdup(str);        /* uses strdup, the new node gets its own copy of str, doesn't rely on orginal memory */
 
-	if (newNode->str == NULL)
+	if (newNode->str == NULL)          /* if the str duplication fails */
 	{
 		free(newNode);
 		return (NULL);
 	}
 
-	newNode->len = _strlen(newNode->str);
-	newNode->next = NULL;
-
-	if (*head == NULL)
+	newNode->len = _strlen(newNode->str);      /* calculates the length of new str and stores it in the len member of new node*/
+	newNode->next = NULL;     /* initializes the next ptr of the new node to NULL, because this is gonna be the last element in the list*/
+        
+	/* head is a ptr to head ptr, *head is head ptr itself, *head == NULL is checking if the list is empty */
+	if (*head == NULL)                        
 	{
-		*head = newNode;
+		*head = newNode;            /* if the list is empty, set the new node as head ptr and return */
 		return (newNode);
 	}
 
-	temp = *head;
-	while (temp->next != NULL)
+	temp = *head;                       /* set temp as the head ptr(temp now points to the first node of the list)*/
+	while (temp->next != NULL)         
 	{
-		temp = temp->next;
+		temp = temp->next;          /* temp moves along the list until reaches the last node */
 
 	}
-	temp->next = newNode;
+	temp->next = newNode;               /* once loop finishes, temp points to the last node in the list */
 	return (newNode);
 }
