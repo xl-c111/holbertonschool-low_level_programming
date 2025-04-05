3e5e668606f388113af2fb5bab3b703956d7c6d3
xl-c111 <10430@holbertonstudents.com>
2025-04-05 16:29:57 +0000

    fix - forget to update *h by assigning it to newNode when idx is 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,6 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		newNode->next = *h;
 		if (*h != NULL)
 			(*h)->prev = newNode;
+		*h = newNode;
 		return (newNode);
 	}
 	currentNode = *h;
