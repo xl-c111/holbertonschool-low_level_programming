23942234cb4c03cfcfd0d07263b0fd46d84be42a
xl-c111 <10430@holbertonstudents.com>
2025-04-05 17:25:01 +0000

    initialize unsigned i to 0

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,7 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int i, j;
+	unsigned int i = 0, j;
 	dlistint_t *temp;
 
 	if (head == NULL || *head == NULL)
