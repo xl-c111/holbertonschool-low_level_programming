594d7b75e933b4591e8d230e19b5c6b35508153a
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-12 02:10:55 +1000

    Update 0-binary_to_uint.c
    converts a char to int 
    i = i * 2 + ( 'b' - '0')
    - (*b - '0') converts a binary digit char('0' or '1') into an int(0 or 1); 
    - i = i * 2 +... shift the current result i left by one place == multiplying the number by 2

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,10 +7,13 @@
  * Return: the converted number, or 0 if:
  *         - there is one or more chars in the string b that is not 0 or 1
  *         - b is NULL
+ * Description: - (*b - '0') converts a binary digit char('0' or '1') into an int(0 or 1); 
+ *              - i = i * 2 +... shift the current result i left by one place
+ *                               - which means each binary bit moves towards the higher bit == multiplying the number by 2                             
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
+	unsigned int i = 0;  /* initialize the variable to 0, used to accumulate the final result */
 
 	if (b == NULL)
 		return (0);
@@ -18,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		i = i * 2 + (*b - '0');
+		i = i * 2 + (*b - '0');  
 		b++;
 	}
 	return (i);
