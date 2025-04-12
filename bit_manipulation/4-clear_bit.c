d8a8577857a15fdff5d5d193d612b51c7459a959
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-12 21:01:01 +1000

    Update 4-clear_bit.c
    clear the bit at the given index
    - mask = ~(1UL << index);  making a mask with 1 at index, ~(...)bitwise NOT, flips all bits, so the target bit 0, other bits 1
    
    - *n = *n & mask; bitwise AND between *n and mask, target bit becomes 0, all other bits remain unchanged

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * clear_bit - clears the value of a bit at a given index
+ * clear_bit - set the bit value at a given index pointed to by n to 0
  * @n: a pointer to the unsigned long int in which to claer the bit
  * @index: the given index
  * Return: 1 if it worked, or -1 if an error occurred
+ * Description: - mask = ~(1UL << index); (1UL << index) shift the number 1 left by index positions, making a mask with 1 at index
+ *                                        ~(...)bitwise NOT, flips all bits, so the target bit 0, other bits 1
+ *              - *n = *n & mask; bitwise AND between *n and mask, target bit becomes 0, all other bits remain unchanged
  *
  */
 int clear_bit(unsigned long int *n, unsigned int index)
