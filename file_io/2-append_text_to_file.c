4476da27d19fdb44e50402038049d50a8394975f
xl-c111 <10430@holbertonstudents.com>
2025-04-13 16:11:56 +0000

    fix spell error

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -39,7 +39,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_contnet != NULL)
+	if (text_content != NULL)
 	{
 		written_bytes = write(fd, text_content, _strlen(text_content));
 
